a0c9c0d8d3416d4020ab4b1a233dd5eaf4b66271
Mark Karavan <mark.karavan@analogfolk.com>
2019-05-06 16:08:17 -0400

    Adds avgNote function

--- src/main.hs
+++ src/main.hs
@@ -4,7 +4,8 @@ import Helpers
 import Euterpea
 
 main =
-  play $ times 3 instrumentSample
+  -- play $ times 3 instrumentSample
+  play bottomRow
 
 instrumentSample =
   topSample :=: bottomSample
@@ -12,11 +13,21 @@ instrumentSample =
 topSample =
   instrument Sitar $
   c 4 qn :+: c 4 qn :+: d 4 qn :+: c 4 qn
-  -- note qn ((C, 4) :: Pitch)
-  -- play $ times 3 $ c 4 qn
-  -- play $ c 4 qn
 
 bottomSample =
   instrument Woodblock $
   g 3 en :+: g 3 en :+: g 3 en :+: g 3 en :+:
   g 3 en :+: g 3 en :+: a 3 en
+
+
+bottomRow =
+  instrument Woodblock $
+  avgNote (c, 4, en) 8 [0,1,2,3,4,5,6,7]
+
+-- avgNote :: (b -> GHC.Real.Ratio Integer -> Music a, b, GHC.Real.Ratio Integer) -> Dur -> [Dur] -> Dur -> Music a
+avgNote (n, o, nl) al [x,y] = n o nl :+: getRest y al nl :+: n o nl
+avgNote (n, o, nl) al (x:y:ys) = n o nl :+: getRest x y nl :+: avgNote (n, o, nl) al (y:ys)
+
+-- getRest :: Dur -> Dur -> Dur -> Music a
+getRest current next dur =
+  rest $ ((next - current) / 4) - dur
